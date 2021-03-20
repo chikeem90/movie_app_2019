cc78aaff771de8c50ff2a1de591895cf2a10015c
chikeem90 <chikeem90@gmail.com>
2021-03-20 15:55:55 +0900

    #3.1 All you need to know about State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
@@ -7,13 +6,14 @@ class App extends React.Component {
   }
 
   add = () => {
-    console.log("add");
+    this.setState(current => ({count : current.count + 1 }));
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({count : current.count - 1 }));
   };
   
   render() {
+    console.log("render");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
